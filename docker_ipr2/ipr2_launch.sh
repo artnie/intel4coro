74441a101afe2b7a157ac717b8d65c9c17dc1886
bhavyawadhwa856 <bhavyawadhwa81@gmail.com>
2022-07-05 11:23:10 +0200

    docker_ipr2 removed

--- docker_ipr2/ipr2_launch.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-export ROS_PACKAGE_PATH=/home/ros/src:/opt/ros/noetic/share:/opt/ros/noetic/stacks
-export CMAKE_PREFIX_PATH=/home/ros/devel:/opt/ros/noetic
-source /opt/ros/noetic/setup.bash
-source /home/ros/devel/setup.bash
-
-roslaunch iai_pr2_sim ros_control_sim_with_base.launch
