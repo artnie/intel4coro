49c90366db6b29ed03a98f8ccfb1b77dc43660e5
bhavyawadhwa856 <bhavyawadhwa81@gmail.com>
2022-08-17 09:55:37 +0200

    updated

--- docker_giskard/giskard_launch.sh
+++ docker_giskard/giskard_launch.sh
@@ -1,11 +1,16 @@
 #!/bin/bash
 
-
-export ROS_PACKAGE_PATH=/home/ros/src:/opt/ros/noetic/share:/opt/ros/noetic/stacks
-export CMAKE_PREFIX_PATH=/home/ros/devel:/opt/ros/noetic
+#Sourcing
+export ROS_PACKAGE_PATH=/home/ros/giskard_ws/src:/opt/ros/noetic/share:/opt/ros/noetic/stacks
+export CMAKE_PREFIX_PATH=/home/ros/giskard_ws/devel:/opt/ros/noetic
 source /opt/ros/noetic/setup.bash
-source /home/ros/devel/setup.bash
+source /home/ros/giskard_ws/devel/setup.bash
 
+#Setting env variables 
+export XDG_RUNTIME_DIR=/run/user/1002
+export ROS_ROOT=/opt/ros/noetic/share/ros
+export ROS_PACKAGE_PATH=/home/ros/giskard_ws/src/giskard_msgs:/home/ros/giskard_ws/src/giskardpy:/home/ros/giskard_ws/src/qpoases:/opt/ros/noetic/share
 
-roslaunch giskardpy giskardpy_pr2.launch --wait
-#rosrun rviz rviz -d `rospack find package_name`/rviz/config_file.rviz
+#launch giskard and rviz
+roslaunch giskardpy giskardpy_pr2.launch --wait &
+rviz
--- docker_ipr2/ipr2_launch.sh
+++ docker_ipr2/ipr2_launch.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
-
-export ROS_PACKAGE_PATH=/home/ros/src:/opt/ros/noetic/share:/opt/ros/noetic/stacks
-export CMAKE_PREFIX_PATH=/home/ros/devel:/opt/ros/noetic
+export ROS_PACKAGE_PATH=/home/ros/iai_pr2_sim_ws/src:/opt/ros/noetic/share:/opt/ros/noetic/stacks
+export CMAKE_PREFIX_PATH=/home/ros/iai_pr2_sim_ws/devel:/opt/ros/noetic
 source /opt/ros/noetic/setup.bash
-source /home/ros/devel/setup.bash
+source /home/ros/iai_pr2_sim_ws/devel/setup.bash
 
+
+#Setting env variables
+export ROS_ROOT=/opt/ros/noetic/share/ros
+export PYTHONPATH=/home/ros/iai_pr2_sim_ws/devel/lib/python3/dist-packages:/opt/ros/noetic/lib/python3/dist-packages
+
+#launch pr2
 roslaunch iai_pr2_sim ros_control_sim_with_base.launch
